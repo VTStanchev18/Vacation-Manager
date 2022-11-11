f016e660eed5d513aa172c89ba594a54972d0f52
VTStanchev18 <VTStanchev18@codingburgas.bg>
2022-11-11 11:49:24 +0200

    Add ASP project with MVC architecture

---
+++ VacationManager MVC/VacationManager/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vacation_Manager.VacationManager;
+
+public partial class Team
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? ProjectId { get; set; }
+
+    public virtual Project? Project { get; set; }
+
+    public virtual ICollection<Project> Projects { get; } = new List<Project>();
+
+    public virtual ICollection<UserTeam> UserTeams { get; } = new List<UserTeam>();
+}
---
+++ VacationManager MVC/VacationManager/UserTeam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vacation_Manager.VacationManager;
+
+public partial class UserTeam
+{
+    public int LeaderId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? TeamId { get; set; }
+
+    public virtual Team? Team { get; set; }
+
+    public virtual User? User { get; set; }
+}
